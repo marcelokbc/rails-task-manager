e7a1ef0aa1302d6bba717b36e2ee6a8c75899e49
Marcelo <marcelo.cansilva@gmail.com>
2021-04-10 22:13:50 -0300

    Error update task

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,26 @@
 Rails.application.routes.draw do
-  root to:"tasks#index"
+  #index route
+  root to: "tasks#index"
+
   get "tasks", to: "tasks#index"
+  #create route
   get "tasks/new", to: "tasks#new"
+
+  #display one task route
   get "tasks/:id", to: "tasks#show", as: :task
+
+  # post new item
   post "tasks", to: "tasks#create"
+
+  # get edit to item
+  get "tasks/:id/edit", to: "tasks#edit"
+
+  #edit to item
+  patch "tasks/:id", to: "tasks#update"
+
+  #destroy
+  delete "tasks/:id", to: "tasks#destroy"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
