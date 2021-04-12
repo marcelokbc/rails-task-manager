db493d34d278be34432e18daff5ebe3021883497
Marcelo <marcelo.cansilva@gmail.com>
2021-04-12 12:48:17 -0300

    Fix index, show, _form, edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,24 @@
 Rails.application.routes.draw do
-  #index route
+  # index route
   root to: "tasks#index"
 
-  get "tasks", to: "tasks#index"
-  #create route
-  get "tasks/new", to: "tasks#new"
+  get "tasks", to: "tasks#index", as: :tasks
+  # create route
+  get "tasks/new", to:"tasks#new", as: :new_task
 
-  #display one task route
-  get "tasks/:id", to: "tasks#show", as: :task
+  # display one task route
+  get"tasks/:id", to:"tasks#show", as: :task
 
   # post new item
   post "tasks", to: "tasks#create"
 
   # get edit to item
-  get "tasks/:id/edit", to: "tasks#edit"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
 
-  #edit to item
+  # edit to item
   patch "tasks/:id", to: "tasks#update"
 
-  #destroy
-  delete "tasks/:id", to: "tasks#destroy"
+  # destroy
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy_task
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-
